ec7a7efffc580826ab4c19d5ab2a2b0bf10a7c20
andhess <andrewclaytonhess@gmail.com>
2014-01-31 00:51:25 -0500

    moving things around

--- main.py
+++ main.py
@@ -2,18 +2,43 @@ import sys
 import datetime
 import math
 import gc
+import numpy as np
 #import scipy.stats
 from read import parse
+from analysis import sort
 
 
 def main( filePath ):
+
+    responses = ([ 'export', 'exit', 'sort'])
+
     query = parse.fileParse( filePath )
     query.readQuoraData()
-    gc.collect()
-    print 'Successfully imported all data from CSV'
     print query
+    data = query.data
+    headers = query.headers
+    gc.collect()
 
-    
+    while True:
+
+        job = newCommand()
+
+
+
+        if job == 'False'
+
+
+        if job == 'exit'
+            sys.exit()
+
+
+
+    print 'Successfully imported all data from CSV'
+
+
+
+
+
 
 if __name__ == "__main__":
     main( sys.argv[1] )
